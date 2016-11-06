4f783c42aa4a9633962cefd74fa1d9daad855695
Eran Machlev <eran.machlev@gmail.com>
2016-11-06 11:34:58 +0200

    Fix next lesson original name in dashboard

--- app/src/main/java/com/ohelshem/app/android/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/ohelshem/app/android/dashboard/DashboardFragment.kt
@@ -156,7 +156,7 @@ class DashboardFragment : BaseMvpFragment<DashboardView, DashboardPresenter>(), 
             if (!isFuture) {
                 changes?.forEach {
                     if (it.hour - 1 == data.hour.hourOfDay) {
-                        lessonName.text = ("<b>" + it.content + "</b> (" + instead + " " + data.hour.name + ")").fromHtml()
+                        lessonName.text = ("<b>" + it.content + "</b> (" + instead + " " + data.nextHour.name + ")").fromHtml()
                         currentLesson.backgroundColor = it.color
                         lessonName.textColor = Color.WHITE
                         timeLeft.textColor = Color.WHITE
