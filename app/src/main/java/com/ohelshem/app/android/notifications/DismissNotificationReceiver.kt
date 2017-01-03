8d55df4c7cdd140526174bb2815b7a0762374c7f
Eran Machlev <eran.machlev@gmail.com>
2017-01-03 14:30:23 +0200

    Fix ongoing notification bug -.-

--- app/src/main/java/com/ohelshem/app/android/notifications/DismissNotificationReceiver.kt
+++ app/src/main/java/com/ohelshem/app/android/notifications/DismissNotificationReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import com.github.salomonbrys.kodein.LazyKodein
 import com.github.salomonbrys.kodein.android.appKodein
 import com.github.salomonbrys.kodein.erased.instance
+import com.ohelshem.app.clearTime
 import com.ohelshem.app.controller.storage.Storage
 import org.jetbrains.anko.notificationManager
 import java.util.*
@@ -15,21 +16,13 @@ class DismissNotificationReceiver(): BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
         val kodein = LazyKodein(context.appKodein)
-        val storage = kodein.instance<Storage>()
-        storage.value.ongoingNotificationDisableDate = toDayOnly(Calendar.getInstance()).timeInMillis
+        val storage = kodein.instance<Storage>().value
+        storage.ongoingNotificationDisableDate = Calendar.getInstance().clearTime().timeInMillis
         context.notificationManager.cancel(NotificationId)
     }
 
     companion object {
         private const val NotificationId = 1342
-    }
-
-    fun toDayOnly(cal: Calendar): Calendar {
-        cal.set(Calendar.HOUR,0)
-        cal.set(Calendar.MINUTE,0)
-        cal.set(Calendar.SECOND,0)
-        cal.set(Calendar.MILLISECOND,0)
-        return cal
     }
 
 }
